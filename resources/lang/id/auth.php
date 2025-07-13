0465d8da563894005ddf1baeff87f7b485a003fb
mohamadamirs <199529218+mohamadamirs@users.noreply.github.com.>
2025-07-14 00:20:35 +0700

    update & perbaikan

--- resources/lang/id/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'failed'   => 'Kredensial ini tidak cocok dengan catatan kami.',
-    'password' => 'Kata sandi yang diberikan salah.',
-    'throttle' => 'Terlalu banyak upaya login. Silakan coba lagi dalam :seconds detik.',
-
-];
